59d4bf488d4b51603185d17c36123accef09c026
Thejas <teju270934@gmail.com>
2024-04-17 19:56:40 +0530

    done with the coding challange-2 based on function and arrays

--- Fundamentals-2/challanges.js
+++ Fundamentals-2/challanges.js
@@ -27,12 +27,6 @@ Test data:
 § Data 1: Dolphins score 44, 23 and 71. Koalas score 65, 54 and 49
 § Data 2: Dolphins score 85, 54 and 41. Koalas score 23, 34 and 27
 
-
-Hints:
-§ To calculate average of 3 values, add them all together and divide by 3
-§ To check if number A is at least double number B, check for A >= 2 * B. 
-Apply this to the team's average scores �
-GOOD LUCK 
 */
 
 // general arrow function to find the average of any team 
@@ -52,3 +46,50 @@ function checkWinner(dolphinAvg,koalasAvg){
         console.log(`It's a draw. No one wins`) 
 }
  checkWinner(avg(85,54,41),avg(85,54,41));
+
+
+ /*
+Coding Challenge #2
+
+Steven is still building his tip calculator, using the same rules as before: Tip 15% of 
+the bill if the bill value is between 50 and 300, and if the value is different, the tip is 
+20%.
+
+
+Your tasks:
+1. Write a function 'calcTip' that takes any bill value as an input and returns 
+the corresponding tip, calculated based on the rules above (you can check out 
+the code from first tip calculator challenge if you need to). Use the function 
+type you like the most. Test the function using a bill value of 100
+
+2. And now let's use arrays! So create an array 'bills' containing the test data 
+below
+
+3. Create an array 'tips' containing the tip value for each bill, calculated from 
+the function you created before
+
+4. Bonus: Create an array 'total' containing the total values, so the bill + tip
+
+Test data: 125, 555 and 44
+
+ */
+
+function calcTip(bill){
+    if(bill >=50 && bill <= 300)
+        return bill * 0.15;
+    if(bill > 300)
+        return bill * 20;
+    return bill;
+}
+
+console.log(`The tip is : ${calcTip(100)}`);
+
+const bills = new Array(125,555,44);
+const tips = new Array(calcTip(125),calcTip(555),calcTip(44));
+const total = new Array(bills[0]+ tips[0] ,bills[1]+tips[1],tips[tips.length-1]+bills[bills.length-1]);
+
+console.log(`The bill : ${bills}
+
+The tip : ${tips}
+
+The total (bill + total) ; ${total}`);
