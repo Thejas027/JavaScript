b452e219af6d54d1fde4b6735acfd08a47da13a9
Thejas <teju270934@gmail.com>
2024-04-25 13:25:21 +0530

    coding challange-3 Done

--- Coding-Challanges/DataStructres/script.js
+++ Coding-Challanges/DataStructres/script.js
@@ -172,3 +172,56 @@ for (const goals of game.scored)
   scorers[goals] ? scorers[goals]++ : (scorers[goals] = 1);
 
 console.log(scorers);
+
+/*
+Coding Challenge #3
+
+Let's continue with our football betting app! This time, we have a map called 
+'gameEvents' (see below) with a log of the events that happened during the 
+game. The values are the events themselves, and the keys are the minutes in which 
+each event happened (a football game has 90 minutes plus some extra time).
+
+Your tasks:
+1. Create an array 'events' of the different game events that happened (no 
+duplicates)
+
+2. After the game has finished, is was found that the yellow card from minute 64 
+was unfair. So remove this event from the game events log.
+
+3. Compute and log the following string to the console: "An event happened, on 
+average, every 9 minutes" (keep in mind that a game has 90 minutes)
+
+4. Loop over 'gameEvents' and log each element to the console, marking 
+whether it's in the first half or second half (after 45 min) of the game, like this:
+[FIRST HALF] 17: ⚽ GOAL
+
+*/
+const gameEvents = new Map([
+  [17, '⚽ GOAL'],
+  [36, '🔁 Substitution'],
+  [47, '⚽ GOAL'],
+  [61, '🔁 Substitution'],
+  [64, '🟡 Yellow card'],
+  [69, '🔴 Red card'],
+  [70, '🔁 Substitution'],
+  [72, '🔁 Substitution'],
+  [76, '⚽ GOAL'],
+  [80, '⚽ GOAL'],
+  [92, '🟡 Yellow card'],
+]);
+console.log('\n\n---------coding challange -3')
+//1.solution
+const events = new Set(gameEvents.values());
+console.log(events);
+
+//2.solution
+gameEvents.delete(64);
+console.log(gameEvents);
+//3.solution 
+const avg = 90 / gameEvents.size;
+console.log(`An event had happened on average , every ${avg} minutes`);
+//4.solution 
+for (const [min, event] of gameEvents) {
+  const half = min <= 45 ? 'FIRST' : 'SECOND';
+  console.log(`[${half} HALF] : ${event}`);
+}
