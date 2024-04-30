a539f440110bf072c4abc1f697da03fbe48aa9e8
Thejas <teju270934@gmail.com>
2024-04-30 17:42:32 +0530

    smooth scrolling of a page added

--- Projects/BankistMarketPage/script.js
+++ Projects/BankistMarketPage/script.js
@@ -1,1 +1,68 @@
 'use strict';
+
+const modal = document.querySelector('.modal');
+const overlay = document.querySelector('.overlay');
+const btnCloseModal = document.querySelector('.btn--close-modal');
+const btnsOpenModal = document.querySelectorAll('.btn--show-modal');
+
+const openModal = function (e) {
+  e.preventDefault();
+  modal.classList.remove('hidden');
+  overlay.classList.remove('hidden');
+};
+
+const closeModal = function () {
+  modal.classList.add('hidden');
+  overlay.classList.add('hidden');
+};
+
+btnsOpenModal.forEach(btn => btn.addEventListener('click', openModal));
+
+btnCloseModal.addEventListener('click', closeModal);
+overlay.addEventListener('click', closeModal);
+
+document.addEventListener('keydown', function (e) {
+  if (e.key === 'Escape' && !modal.classList.contains('hidden')) {
+    closeModal();
+  }
+});
+
+// smooth scrolling of learn more button
+
+const btnScrollTo = document.querySelector('.btn--scroll-to');
+const section1 = document.querySelector('#section--1');
+
+btnScrollTo.addEventListener('click', function (e) {
+  //get the cor-ordinates
+  const s1coords = section1.getBoundingClientRect();
+
+  // some methods that return co-ordinates of page
+  // console.log(s1coords);
+  // console.log(e.target.getBoundingClientRect());
+  // console.log('current scroll (X/Y) : ', window.pageXOffset, window.pageYOffset);
+  // console.log(
+  //   'height and width of current port : ',
+  //   document.documentElement.clientHeight,
+  //   document.documentElement.clientWidth
+  // );
+
+  //method1
+  // window.scrollTo(
+  //   s1coords.left + window.pageXOffset,
+  //   s1coords.top + window.pageYOffset
+  // );
+
+  //method2
+  // window.scrollTo({
+  //   left: s1coords.left + window.pageXOffset,
+  //   top: s1coords.top + window.pageYOffset,
+  //   behavior: 'smooth',
+  // });
+
+  section1.scrollIntoView({ behavior: 'smooth' }); 
+});
+
+const h1 = document.querySelector('h1');
+h1.addEventListener('mouseenter', function () {
+  alert('added event listener');
+});
