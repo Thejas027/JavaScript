df7c4c2a5a2ae79fe92e21b18a079ab35001b123
Thejas <teju270934@gmail.com>
2024-05-02 13:43:28 +0530

    another coding challange completed on opps

--- Coding-Challanges/OOps/script.js
+++ Coding-Challanges/OOps/script.js
@@ -65,7 +65,7 @@ Test data:
 § Data car 1: 'Ford' going at 120 km/h
 */
 
-//
+
 console.log('\n\n----------coding challange-2');
 class CarEs6 {
   constructor(make, speed) {
@@ -102,3 +102,51 @@ BMW.brake();
 const benz = new Car('BENZ', 100);
 benz.speedus = 150;
 console.log(benz.speedus);
+
+/*
+Coding Challenge #3
+
+Your tasks:
+1. Use a constructor function to implement an Electric Car (called 'EV') as a child
+"class" of 'Car'. Besides a make and current speed, the 'EV' also has the 
+current battery charge in % ('charge' property)
+
+2. Implement a 'chargeBattery' method which takes an argument 
+'chargeTo' and sets the battery charge to 'chargeTo'
+
+3. Implement an 'accelerate' method that will increase the car's speed by 20, 
+and decrease the charge by 1%. Then log a message like this: 'Tesla going at 140 
+km/h, with a charge of 22%'
+
+
+Test data:
+ Data car 1: 'Tesla' going at 120 km/h, with a charge of 23%
+*/
+
+console.log('\n\n---Coding challange -3');
+
+
+const EV = function (make, speed, charge) {
+  Car.call(this, make, speed);
+  this.charge = charge;
+};
+
+//linking the prototype
+EV.prototype = Object.create(Car.prototype);
+
+EV.prototype.chargeBattery = function (chargeTo) {
+  this.charge = chargeTo;
+};
+
+EV.prototype.accelerate = function () {
+  this.speed += 20;
+  this.charge--;
+  console.log(
+    `Tesla is going at a speed of ${this.speed} km/h with a charge of ${this.charge} %`
+  );
+};
+
+const tesla = new EV('Tesla', 120, 23);
+// tesla.chargeBattery(90);
+console.log(tesla);
+tesla.accelerate();
