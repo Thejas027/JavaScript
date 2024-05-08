6c60087412dcf573a5d941f9273c9236fe14e439
Thejas <teju270934@gmail.com>
2024-05-08 20:58:16 +0530

    modified the script js file and added a clean js file where the code in clean js changed to write in modern way

--- ModrenScript/clean.js
+++ ModrenScript/clean.js
@@ -1,3 +1,5 @@
+/*
+
 var budget = [
   { value: 250, description: 'Sold old TV 📺', user: 'jonas' },
   { value: -45, description: 'Groceries 🥑', user: 'jonas' },
@@ -62,3 +64,59 @@ var bigExpenses = function (limit) {
   output = output.slice(0, -2); // Remove last '/ '
   console.log(output);
 };
+
+
+*/
+
+//////////////////////////
+
+// the above code changed to modern js, below code shows the changes
+
+const budget = [
+  { value: 250, description: 'Sold old TV 📺', user: 'jonas' },
+  { value: -45, description: 'Groceries 🥑', user: 'jonas' },
+  { value: 3500, description: 'Monthly salary 👩‍💻', user: 'jonas' },
+  { value: 300, description: 'Freelancing 👩‍💻', user: 'jonas' },
+  { value: -1100, description: 'New iPhone 📱', user: 'jonas' },
+  { value: -20, description: 'Candy 🍭', user: 'matilda' },
+  { value: -125, description: 'Toys 🚂', user: 'matilda' },
+  { value: -1800, description: 'New Laptop 💻', user: 'jonas' },
+];
+
+const spendingLimits = {
+  jonas: 1500,
+  matilda: 100,
+};
+
+const getLimit = user => spendingLimits?.[user] ?? 0;
+
+const addExpense = function (value, description, user = 'jonas') {
+  user = user.toLowerCase();
+
+  if (value <= getLimit(user)) {
+    budget.push({ value: -value, description, user });
+  }
+};
+addExpense(10, 'Pizza 🍕');
+addExpense(100, 'Going to movies 🍿', 'Matilda');
+addExpense(200, 'Stuff', 'Jay');
+
+const check = function () {
+  for (const entry of budget)
+    if (entry.value < -getLimit(entry.user)) {
+      entry.flag = 'limit';
+    }
+};
+check();
+
+const lobBigExpenses = function (bigLimit) {
+  let output = '';
+  for (const entry of budget)
+    output +=
+      entry.value <= -bigLimit ? `${entry.description.slice(-2)} / ` : '';
+
+  output = output.slice(0, -2); // Remove last '/ '
+  console.log(output);
+};
+
+console.log(budget);
--- ModrenScript/script.js
+++ ModrenScript/script.js
@@ -21,6 +21,69 @@
 //////////////////////////
 // accessing the default values
 
-import add from './shoppingCart.js';
+// import add from './shoppingCart.js';
 
-add('bread', 7);
+// add('bread', 7);
+
+////////
+
+//async function x (){} ---> actuall use of asynchronous method
+
+// // method to use the top level await
+// const res = await fetch(`https://jsonplaceholder.typicode.com/posts`);
+// const data = await res.json();
+// console.log(data);
+// console.log
+
+const getLastPost = async function () {
+  const res = await fetch(`https://jsonplaceholder.typicode.com/posts`);
+  const data = await res.json();
+  return { title: data.at(-1).title, body: data.at(-1).body };
+};
+
+const lastPost = getLastPost();
+
+// not very clear
+// lastPost.then(last => console.log(last));
+
+const lastPost2 = await getLastPost();
+console.log(lastPost2);
+
+// the below function will be able to call only once , main moto of creating this function is that not to use the code multiple time
+
+const shoppingCart2 = function () {
+  const cart = [];
+  const shppingCost = 10;
+  const totalQuantity = 9;
+  const totalPrice = 237;
+
+  const addToCart = function (product, quantity) {
+    cart.push({ product, quantity });
+    console.log(product, quantity + 'added to cart');
+  };
+  const orderStock = function (product, quantity) {
+    console.log(`${product} ${quantity} ordered form supplier`);
+  };
+  return {
+    addToCart,
+    cart,
+    totalQuantity,
+    totalPrice,
+  };
+};
+
+shoppingCart2.addToCart('bread', 4);
+shoppingCart2.addToCart('pizza', 5);
+
+console.log(shoppingCart2);
+console.log(shoppingCart2.totalPrice); // it gives undefined where its a block scope,
+
+// // using of common js, which only works in node.js
+
+// // export
+// export.addedToCart = function(){
+//   console.log('item added to cart');
+// }
+
+// //import
+// const { addToCart } = require('./shoppingCart');
